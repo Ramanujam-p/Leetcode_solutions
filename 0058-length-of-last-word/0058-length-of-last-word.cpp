d9c26fe3d5a434c5aa81c0e9733fc4dbd22dcdd4
RAMANUJAM P <pramanujam68@gmail.com>
2025-08-29 18:39:34 +0530

    Time: 0 ms (100%), Space: 8.8 MB (90.42%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int l=0,i=s.size()-1;
+        while(i>=0 && s[i]==' ')
+            {
+                i--;
+            }
+        while(i>=0 && s[i]!=' ')
+            {
+                i--;
+                l++;
+            }
+        return l;
+    }
+};
