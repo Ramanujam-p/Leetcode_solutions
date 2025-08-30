3891e67267e162dd29e1b7895d89c36269ba1ba5
RAMANUJAM P <pramanujam68@gmail.com>
2025-08-30 09:40:12 +0530

    Time: 52 ms (49.72%), Space: 90.7 MB (71.96%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> s;
+        for(int a:nums)
+        {
+            if(s.count(a))
+                return true;
+            s.insert(a);
+        }
+        return false;
+    }
+};
