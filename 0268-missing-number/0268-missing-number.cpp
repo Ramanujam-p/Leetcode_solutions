941a3baca53e7e30e69440e72bdb851968357d67
RAMANUJAM P <pramanujam68@gmail.com>
2025-09-27 08:59:09 +0530

    Time: 0 ms (100%), Space: 21.8 MB (70.22%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size(),s=0,f=n*(n+1)/2;
+        for(int i=0;i<n;i++) s+=nums[i];
+        return f-s;
+    }
+};
