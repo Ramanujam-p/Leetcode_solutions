2eadfc977b62fbcd2206dd6ae8ab14f29d320808
RAMANUJAM P <pramanujam68@gmail.com>
2025-10-11 11:07:51 +0530

    Time: 2 ms (2.74%), Space: 7.9 MB (48.28%) - LeetHub

---
+++ 2507-number-of-common-factors/2507-number-of-common-factors.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int commonFactors(int a, int b) {
+        int c = 0;
+        int m = max(a,b);
+        for(int i=1;i<=m;i++)
+        {
+            if(a%i==0)
+            {
+                if(b%i==0)
+                    c++;  
+            }
+        }
+        return c;
+    }
+};
