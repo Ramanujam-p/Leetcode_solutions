bf539e8790c2221ca9ac7810775ed8b4068f3562
RAMANUJAM P <pramanujam68@gmail.com>
2025-10-22 18:47:43 +0530

    Time: 0 ms (100%), Space: 8.6 MB (66.49%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int n1 = s.size(),n2 = t.size(), i = 0 , j = 0;
+        while(i<n1 && j<n2)
+        {
+            if(s[i]==t[j]) i++;
+            j++;
+        }
+        return i == n1;
+    }
+};
