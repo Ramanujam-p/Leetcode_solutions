32737d19772baaea58a32092c44c7712b19d878e
RAMANUJAM P <pramanujam68@gmail.com>
2025-12-29 20:47:54 +0530

    Time: 3 ms (71.45%), Space: 16.1 MB (24.24%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
     vector<string> r;
-    bool val(string s)
-    {
-        int a = 0;
-        for(char c:s)
-        {
-            if(c=='(') a++;
-            else a--;
-            if(a<0) return false;
-        }
-        return a==0;
-    }
-    void gen(string s,int n)
+    void gen(string s,int o,int c,int n)
     {
         if(s.length()==2*n)
         {
-            if(val(s))
-                r.push_back(s);
+            r.push_back(s);
             return;
         }
-        gen(s+"(",n);
-        gen(s+")",n);
+        if(o<n)
+            gen(s+"(",o+1,c,n);
+        if(c<o)
+            gen(s+")",o,c+1,n);
     }
     vector<string> generateParenthesis(int n) {
-        gen("",n);
+        gen("",0,0,n);
         return r;
+
     }
 };
