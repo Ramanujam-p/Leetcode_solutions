4bf27fbf8509e93e8f9e79da7b8b169b68aa3d0e
RAMANUJAM P <pramanujam68@gmail.com>
2026-01-01 09:55:36 +0530

    Time: 0 ms (100%), Space: 11.3 MB (92.86%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> plusOne(vector<int>& digits) {
         for(int i=digits.size()-1;i>=0;i--)
         {
-            if (digits[i] < 9)
+            if(digits[i]<9)
             {
                 digits[i]++;
                 return digits;
