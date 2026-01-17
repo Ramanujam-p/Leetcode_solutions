a2b03e167d710617871152857c23dfb786c3d2d6
RAMANUJAM P <pramanujam68@gmail.com>
2026-01-17 17:13:10 +0530

    Time: 0 ms (100%), Space: 52.1 MB (99.3%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size(),c=0;
+        for(int i=0;i<n;i++)
+        {
+            if(i>c) return false;
+            c = max(c,i+nums[i]);
+        }
+        return true;
+    }
+};
