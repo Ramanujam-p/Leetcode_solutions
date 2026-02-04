b7beecf29734258283a23d0d35a6d557967de403
RAMANUJAM P <pramanujam68@gmail.com>
2026-02-04 16:27:31 +0530

    Time: 0 ms (100%), Space: 7.7 MB (88.8%) - LeetHub

---
+++ 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int r(int n)
+    {
+        int t=n,r=0;
+        while(t!=0)
+        {
+            int d = t%10;
+            r=(r*10)+d;
+            t/=10;
+        }
+        return r;
+    }
+    bool isSameAfterReversals(int num) {
+         int a = r(num);
+         if(num==r(a)) return true;
+         return false;
+    }
+};
