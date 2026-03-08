c5ec11cadc7774b0aa2a6bf1b6a20be6b62271d2
RAMANUJAM P <pramanujam68@gmail.com>
2026-03-08 11:03:05 +0530

    Time: 356 ms (60.98%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,3 +1,8 @@
 /* Write your PL/SQL query statement below */
-SELECT MAX(SALARY) AS SecondHighestSalary FROM EMPLOYEE
-WHERE SALARY < (SELECT MAX(SALARY) FROM EMPLOYEE);
+
+    SELECT MAX(Salary) AS SecondHighestSalary
+    FROM Employee
+    WHERE Salary < (
+        SELECT MAX(Salary) FROM
+        Employee
+    );
