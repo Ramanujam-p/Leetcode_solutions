73b1939f9c340b9a54d8ea2dd83f3750b2f69a11
RAMANUJAM P <pramanujam68@gmail.com>
2026-04-15 10:47:32 +0530

    Time: 879 ms (17.9%), Space: 0B (100%) - LeetHub

---
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,4 @@
+/* Write your PL/SQL query statement below */
+SELECT contest_id, ROUND(COUNT(R.contest_id) * 100 / (SELECT COUNT(*) FROM Users), 2)  percentage  FROM  Register R  
+GROUP BY contest_id 
+ORDER BY percentage DESC, contest_id;
