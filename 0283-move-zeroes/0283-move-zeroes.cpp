d45b2eef8ade0429b13ac5b21f1a7262cfb8c9d0
RAMANUJAM P <pramanujam68@gmail.com>
2026-05-23 18:34:06 +0530

    Time: 0 ms (100%), Space: 23.8 MB (81.65%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        stable_partition(nums.begin(),nums.end(),[](int x){return x!=0;});
+        int n = nums.size();
+        int l = 0,r=0,h=n-1;
+        while(l<=h && r<=h)
+        {
+            if((nums[l]!=0 && nums[r]!=0) ||nums[r]!=0){
+                swap(nums[l],nums[r]);
+                l++;
+                r++;
+            } 
+            else r++;
+        }
     }
 };
