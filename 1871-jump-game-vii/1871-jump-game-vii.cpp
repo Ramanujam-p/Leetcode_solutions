04637e4dd7f7eca33b067093eeebbd2ffe5f5d92
RAMANUJAM P <pramanujam68@gmail.com>
2026-05-25 09:28:20 +0530

    Time: 7 ms (78.74%), Space: 20 MB (81.41%) - LeetHub

---
+++ 1871-jump-game-vii/1871-jump-game-vii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canReach(string s, int minJump, int maxJump) {
+        int n = s.size();
+        vector<bool> dp(n,false);
+        dp[0]=true;
+        int r=0;
+        for(int i=1;i<n;i++)
+        {
+            if(i-minJump>=0 && dp[i-minJump]) r++;
+            if(i-maxJump-1>=0 && dp[i-maxJump-1]) r--;
+            dp[i]=(r>0 &&s[i]=='0'); 
+        }
+        return dp[n-1];
+    }
+};
