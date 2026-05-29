3b97f6b8abd77080439d53cde29eeeb70ce95322
RAMANUJAM P <pramanujam68@gmail.com>
2026-05-29 12:18:03 +0530

    Time: 0 ms (100%), Space: 27.9 MB (17.62%) - LeetHub

--- 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.cpp
+++ 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int s=0;
-    int digit_sum(int n)
+    int digits_sum(int n)
     {
         s=0;
-        while(n!=0)
+        while(n>0)
         {
             int d = n%10;
             s+=d;
@@ -12,12 +12,18 @@ public:
         }
         return s;
     }
+    int minimum_element(vector<int> &a)
+    {
+        int m = a[0];
+        for(int i=0;i<a.size();i++)
+            if(m>a[i])
+                m=a[i];
+        return m;
+    }
     int minElement(vector<int>& nums) {
         vector<int> a;
-        int n = nums.size();
-        for(int i=0;i<n;i++)
-            a.push_back(digit_sum(nums[i]));   
-        sort(a.begin(),a.end());
-        return a[0];
+        for(int i=0;i<nums.size();i++)
+            a.push_back(digits_sum(nums[i]));
+        return minimum_element(a);
     }
 };
