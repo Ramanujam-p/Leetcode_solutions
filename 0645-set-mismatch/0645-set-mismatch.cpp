0f8f863c90c6d6725cf51f2d5044936fac023d03
RAMANUJAM P <pramanujam68@gmail.com>
2026-06-04 19:14:54 +0530

    Time: 0 ms (100%), Space: 25.2 MB (64.65%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        int n = nums.size();
-        int dup=-1,mis=-1;
-        vector<int> a(n+1,0);
-        for(int b:nums)
-            a[b]++;
-        for(int i=1;i<=n;i++)
+        long long n = nums.size(),actual_sum=0,actual_sum_of_squares=0,expected_sum=(n*(n+1))/2,expected_sum_of_squares=(n*(n+1)*((2*n)+1))/6;
+        for(int i=0;i<n;i++)
         {
-            if(a[i]==2)
-                dup=i;
-            if(a[i]==0)
-                mis=i;
+            actual_sum+=nums[i];
+            actual_sum_of_squares+=nums[i]*nums[i];
         }
-        return{dup,mis};
+        long long x=actual_sum-expected_sum,y=actual_sum_of_squares-expected_sum_of_squares,A=(x+(y/x))/2;
+        return {(int)A,(int)(A-x)};
+
     }
 };
